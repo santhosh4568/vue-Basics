cc3dacf627dcb9b3a49d0502e80e85df8d81d6d3
santhosh4568 <santhoshpcse@gmail.com>
2021-07-05 12:40:32 +0530

    third commit

---
+++ firstapp/src/components/AddTodo.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <form @submit="addTodo">
+            <input type="text" v-model="title" placeholder="Add ToDo">
+            <input type="submit" value="Add" class="btn">
+        </form>
+    </div>
+</template>
+
+
+
+<script>
+
+export default {
+    name:"AddTodo",
+    data(){
+        return{
+            title:''
+        }
+    },
+    methods:
+    {
+        addTodo(e)
+        {
+            e.preventDefault();
+            if(this.title!==''){
+            const newTodo={
+                
+                title:this.title,
+                completed:false,
+            }
+            this.$emit('add-todo',newTodo);
+            this.title='';
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+ form{
+     display: flex;
+ }
+ input[type="text"]{
+     flex:10;padding: 5px;
+ }
+ input[type="submit"]{
+     flex:2;
+ }
+
+</style>
---
+++ firstapp/src/components/TodoItem.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="todo-item" :class="{'is-complete' : todo.completed}">
+       <p>
+           <input type="checkbox" v-on:change="markComplete">
+           {{todo.title}}</p>
+           <button @click="$emit('del-todo',todo.id)" class="del">X</button>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"TodoItem",
+    props:["todo"],
+    methods:{
+        markComplete() {
+            this.todoLocal.completed=!this.todoLocal.completed;
+        }
+    },
+    data()
+    {
+       return{
+           todoLocal:this.todo,
+       };
+    }
+    
+}
+</script>
+
+
+
+<style scoped>
+.todo-item
+  {
+      background: #f4f4f4;
+      padding: 10px;
+      border-bottom: 1px #ccc dotted;
+  }
+  .del
+  {
+      background: #ff0000;
+      color: #fff;
+      border: none;
+      padding: 5px 9px;
+      border-radius: 50%;
+      cursor: pointer;
+      float:right;
+  }
+  .is-complete
+  {
+      text-decoration: line-through;
+  }
+</style>
